e79c5ab50d9323964cdab71cf8e1d98567b6bbfb
EeHeng Chen <eeheng.chen@gmail.com>
2023-08-01 12:11:09 +0000

    added push to kaggle code

---
+++ push_to_kaggle_ds.py
@@ -0,0 +1,18 @@
+import datetime
+import logging
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+if __name__ == "__main__":
+    api = KaggleApi()
+    api.authenticate()
+    # # get dataset
+    # api.dataset_download_files("eehengchen/chen-contrails-2023",
+    #                            path="/tmp",
+    #                            unzip=True)
+    # upload dataset
+    api.dataset_create_version(
+        "./",
+        version_notes=f"Updated on {datetime.datetime.now().strftime('%Y-%m-%d')}",
+    )
+    logging.info(f"Updated eehengchen/chen-contrails-2023 dataset.")
